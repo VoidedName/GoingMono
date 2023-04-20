d9fb0fa05466975da176d874a02fbd580d9d7465
VoidedName <daniel.kogan@gmx.net>
2023-04-20 15:02:34 +0200

    #voided_world + remove comment

--- rust/voided_world/src/r_tree/vizgraph.rs
+++ rust/voided_world/src/r_tree/vizgraph.rs
@@ -14,7 +14,7 @@ impl<T: Coordinate + Display, O: ObjectId + Display> RTree<T, O> {
             None => format!("digraph {} {{\n\tNone;}}", name),
             Some(root) => {
                 format!(
-                    "digraph {} {{\n\tn0 [style=filled, fillcolor=red, fontcolor=white, label=\"node:0\"]\n{}}}",
+                    "digraph {} {{\n\tnode [style=filled, fillcolor=black, fontcolor=white, shape=record]\n\tn0 [label=\"node:0\", fillcolor=red, shape=circle]\n{}}}",
                     name,
                     root.to_vizgraph(0).1
                 )
@@ -25,12 +25,11 @@ impl<T: Coordinate + Display, O: ObjectId + Display> RTree<T, O> {
 
 impl<T: Coordinate + Display, O: ObjectId + Display> Entry<T, O> {
     fn to_vizgraph(&self, label: usize) -> (usize, String) {
-        // let spacing = "\t".repeat(indent);
         let mut result = String::new();
         match self {
             Leaf { children } => {
                 for ObjectRecord(rec, id) in children {
-                    result += format!("\to{} [label=\"{{({}, {}), ({}, {})|object:{}}}\", style=filled, fillcolor=black, fontcolor=white, shape=record];\n",
+                    result += format!("\to{} [label=\"{{({}, {}), ({}, {})|object:{}}}\"];\n",
                                       id,
                                       rec.low.x,
                                       rec.low.y,
@@ -46,7 +45,7 @@ impl<T: Coordinate + Display, O: ObjectId + Display> Entry<T, O> {
                 let mut last_used_label = label;
                 for ChildRecord(rec, child) in children {
                     let child_label = last_used_label + 1;
-                    result += format!("\tn{} [label=\"{{({}, {}), ({}, {})|node:{}}}\", shape=record, style=filled, fillcolor=red, fontcolor=white];\n",
+                    result += format!("\tn{} [label=\"{{({}, {}), ({}, {})|node:{}}}\", fillcolor=red];\n",
                                       child_label,
                                       rec.low.x,
                                       rec.low.y,
